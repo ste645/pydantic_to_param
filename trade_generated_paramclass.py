7649d4eefa3e0e2ef32619153e4707a6863dca1f
Taj <tjgurwara99@gmail.com>
2022-07-11 17:51:56 +0100

    fix: template fix: cleanup

--- trade_generated_paramclass.py
+++ trade_generated_paramclass.py
@@ -6,28 +6,24 @@ import param
 class Period(param.Parameterized):
     """Period Object"""
 
-
-strike = param.Integer(default=None)
-trade_date = param.Integer(default=None)
+    strike= param.Integer(default=None)
+    trade_date= param.Integer(default=None)
 
 
 class Leg(param.Parameterized):
     """Leg Object"""
 
+    strike= param.Integer(default=None)
+    period= param.List(default=None)
 
-strike = param.Integer(default=None)
-period = param.List(default=None)
 
 
 class Trade(param.Parameterized):
     """Trade Object."""
 
-
-strike = param.Number(default=2.0)
-name = param.String(default=None)
-number = param.Integer(default=None)
-
-trade_date = param.Date(default=None)
-
-legs = param.List(default=None)
-periods = param.List(default=None)
+    strike= param.Number(default=2.0)
+    name= param.String(default=None)
+    number= param.Integer(default=None)
+    trade_date= param.Date(default=None)
+    legs= param.List(default=None)
+    periods= param.List(default=None)
